3f3cf8f1d627402234121ba2f9ff744f69bae7b0
Muhammad Tayyab <150431063+gh-tayyab@users.noreply.github.com>
2023-12-11 22:23:31 +0500

    Conditional based exercie

--- README.ts
+++ README.ts
@@ -1,6 +1,5 @@
 # Series-of-Conditional-Tests
 
-
 Question: Conditional Tests: Write a series of conditional tests. Print a statement
 describing each test and your prediction for the results of each test. Your code
 should look something like this:
